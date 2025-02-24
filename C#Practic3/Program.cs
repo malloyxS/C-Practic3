93bc244981d18787938adada8f2827735f140ce0
malloyxS <tropiceskijivan@gmail.com>
2025-02-24 14:57:51 +0300

    Добавьте файлы проекта.

---
+++ C#Practic3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = {-2, 3, 5, -1, 0, 7, -4, 8};
+        int positiveCount = 0;
+
+        foreach (int number in array)
+        {
+            if (number > 0)
+            {
+                positiveCount++;
+            }
+        }
+
+        Console.WriteLine($"Количество положительных чисел в массиве: {positiveCount}");
+    }
+}
